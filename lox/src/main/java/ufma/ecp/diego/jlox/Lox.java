ec1daf93f99bfcf561274984eb3ac218e596d288
DiegoFSX22 <diegodrack1212@gmail.com>
2025-06-22 05:38:29 -0300

    nn

--- lox/src/main/java/ufma/ecp/diego/jlox/Lox.java
+++ lox/src/main/java/ufma/ecp/diego/jlox/Lox.java
@@ -1,3 +1,4 @@
+
 package ufma.ecp.diego.jlox;
 
 import java.io.BufferedReader;
@@ -7,11 +8,11 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Lox {
 
-      static boolean hadError = false;
+     static boolean hadError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -24,7 +25,7 @@ public class Lox {
     }
   }
 
-    private static void runFile(String path) throws IOException {
+ private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
 
@@ -41,11 +42,11 @@ public class Lox {
       String line = reader.readLine();
       if (line == null) break;
       run(line);
-      hadError = false;
+       hadError = false;
     }
   }
 
-    private static void run(String source) {
+  private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
