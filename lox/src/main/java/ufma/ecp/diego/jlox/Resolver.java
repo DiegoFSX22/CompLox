46545577617ded8ab46e1d3fe5b8aee19032b9a7
DiegoFSX22 <diegodrack1212@gmail.com>
2025-07-14 22:03:11 -0300

    Constructors and Initializers

--- lox/src/main/java/ufma/ecp/diego/jlox/Resolver.java
+++ lox/src/main/java/ufma/ecp/diego/jlox/Resolver.java
@@ -17,6 +17,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
     NONE,
     FUNCTION,
+    INITIALIZER,
     METHOD
   }
 
@@ -87,6 +88,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
         for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+          if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
+
       resolveFunction(method, declaration); 
     }
 
@@ -132,6 +137,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.value != null) {
+              if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword,
+            "Can't return a value from an initializer.");
+      }
       resolve(stmt.value);
     }
 
@@ -228,7 +237,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
   public Void visitThisExpr(Expr.This expr) {
-    
+
         if (currentClass == ClassType.NONE) {
       Lox.error(expr.keyword,
           "Can't use 'this' outside of a class.");
