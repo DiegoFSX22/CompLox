a288398f69c3bc3be6870c8690024ade882fb93a
DiegoFSX22 <diegodrack1212@gmail.com>
2025-07-14 21:47:38 -0300

    Properties on Instances

--- lox/src/main/java/ufma/ecp/diego/jlox/Interpreter.java
+++ lox/src/main/java/ufma/ecp/diego/jlox/Interpreter.java
@@ -167,6 +167,20 @@ class Interpreter implements Expr.Visitor<Object>,
     return evaluate(expr.right);
   }
 
+    @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+
+    if (!(object instanceof LoxInstance)) { 
+      throw new RuntimeError(expr.name,
+                             "Only instances have fields.");
+    }
+
+    Object value = evaluate(expr.value);
+    ((LoxInstance)object).set(expr.name, value);
+    return value;
+  }
+
   @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
@@ -269,6 +283,17 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     return function.call(this, arguments);
+  }
+
+    @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name,
+        "Only instances have properties.");
   }
 
   private void checkNumberOperand(Token operator, Object operand) {
